a6a190d3571d3916805883508e05e33c3fe1b9b7
HDNEM <hoanem@gmail.com>
2016-05-01 19:25:42 -0400

    First commit

--- src/Core MVC6/Controllers/HomeController.cs
+++ src/Core MVC6/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Core_MVC6.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,6 +14,14 @@ namespace Core_MVC6.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            ViewBag.sampleTrip = new Trip {
+                ID = 1,
+                Name = "Trip 1",
+                CreatedDate = DateTime.Now,
+                UserName = "Hoan",
+                Stops = null
+            };
+            ViewBag.Message = "Sample Trip";
             return View();
         }
 
---
+++ src/Core MVC6/Models/Trip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_MVC6.Models
+{
+    public class Trip
+    {
+        public int ID { get; set; }
+        [Required]
+        public String Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public String UserName { get; set; }
+        public ICollection<Stop> Stops { get; set; }
+    }
+}
